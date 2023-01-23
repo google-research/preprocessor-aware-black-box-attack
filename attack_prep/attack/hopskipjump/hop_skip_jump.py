9316924703e5da2d681d7fb5b28c11464bd99bfe
Chawin Sitawarin <chawins@berkeley.edu>
2023-01-23 06:29:15 +0000

    Fix bug in hsj

--- attack_prep/attack/hopskipjump/hop_skip_jump.py
+++ attack_prep/attack/hopskipjump/hop_skip_jump.py
@@ -215,6 +215,7 @@ class HopSkipJump(MinimizationAttack):
             if self.stepsize_search == "geometric_progression":
                 # find step size.
                 epsilons = distances / math.sqrt(step + 1)
+                old_epsilons = 1e9
 
                 while True:
                     x_advs_proposals = ep.clip(
@@ -226,6 +227,13 @@ class HopSkipJump(MinimizationAttack):
                     # EDIT: is_adversarial
                     num_queries += 1
                     epsilons = ep.where(success, epsilons, epsilons / 2.0)
+                    # Numerical precision
+                    if ep.all(epsilons == old_epsilons):
+                        epsilons = ep.where(
+                            success, epsilons, ep.zeros_like(epsilons)
+                        )
+                        break
+                    old_epsilons = epsilons
 
                     if ep.all(success):
                         break
@@ -367,6 +375,9 @@ class HopSkipJump(MinimizationAttack):
                 x_advs = ep.astensor(out.detach())
             else:
                 x_advs = ep.astensor(self.preprocess(x_advs.raw))
+        else:
+            # This is needed as x_advs will be modified in-place below
+            x_advs = ep.astensor(x_advs.raw.clone())
 
         # Should rv be re-normalized here? It is not in the original
         # implementation, but it is fixed in later commit:
